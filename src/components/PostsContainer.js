66746935fa936cba0b182868a5608d5a85c29e22
Margaret Berry <codeberry1@gmail.com>
2018-05-06 21:14:57 -0700

    add ability to save new posts

--- src/components/PostsContainer.js
+++ src/components/PostsContainer.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import Post from './Post'
+import Post from './Post';
+import PostForm from './PostForm';
 import axios from 'axios';
+import update from 'immutability-helper';
 
 class PostsContainer extends Component {
   render() {
     return (
       <div>
+        <button className="newPostButton"
+          onClick={this.addNewPost}>
+          New Post
+        </button>
+        <span className="notification">
+          {this.state.notification}
+        </span>
         {this.state.posts.map((post) => {
-          return(<Post post={post} key={post.id} />)
+          if (this.state.editingPostId === post.id) {
+            return(<PostForm post={post} key={post.id}
+              updatePost={this.updatePost}/>)
+          } else {
+            return(<Post post={post} key={post.id} />)
+          }
         })}
       </div>
     )
@@ -16,17 +30,50 @@ class PostsContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      posts: []
+      posts: [],
+      editingPostId: null,
+      notification: ''
     }
   }
 
   componentDidMount() {
     axios.get('http://localhost:3001/posts.json')
     .then(response => {
-      console.log(response)
       this.setState({posts: response.data})
     })
     .catch(error => console.log(error))
+  }
+
+  addNewPost = () => {
+    axios.post(
+      'http://localhost:3001/posts',
+      { post:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      const posts = update(this.state.posts, {
+        $splice: [[0, 0, response.data]]
+      })
+      this.setState({
+        posts: posts,
+        editingPostId: response.data.id
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+  updatePost = (post) => {
+    const postIndex = this.state.posts.findIndex(x => x.id === post.id)
+    const posts = update(this.state.posts, {
+      [postIndex]: { $set: post }
+    })
+      this.setState({posts: posts,
+      notification: 'Your changes have been saved.'
+    })
   }
 }
 
